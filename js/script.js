b13646340c98e7b74708feefe8bb8b9b9b529edc
RafaelFassa <rafael.teixeira0912@gmail.com>
2022-11-02 19:00:12 -0300

    par

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
-$(function() {
-    var staticBackdrop = document.getElementById('exampleModalCenter');
-    var myModal = new bootstrap.Modal(staticBackdrop);
-    myModal.show();
-});
+function numero() {
+    var txtNumber = document.querySelector('input#txtNumber')
+    var res = document.querySelector('div#res')
+    var numero = Number(txtNumber.value)
+    res.innerHTML = `<p> Seu número é ${numero} </p>`
+
+    if (numero % 2 == 0) {
+        $("#exampleModalCenter").modal();
+    } else {
+        res.innerHTML += `<p>Número Impar </p>`
+    }
+}
